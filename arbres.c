cccb87c491e30e9c092090c3e81d3908760f023e
Darenn Keller <keller.darenn@gmail.com>
2016-09-27 20:48:27 +0200

    Implémentation et documentation de noeud_creer.

--- arbres.c
+++ arbres.c
@@ -1,141 +1,94 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <assert.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-# undef NDEBUG
+#include "arbres.h"
 
-# include "arbres.h"
+typedef struct noeud_struct *noeud;
 
-
-typedef struct noeud_struct * noeud ;
-
-
+/*!
+* Cette struct représente un noeud d'un arbre binaire.
+* Elle contient une valeur, un fils gauche (plus petit) et un fils droit. (plus
+* grand)
+*/
 struct noeud_struct {
-  void * val ; 
-  noeud fils_gauche ; 
-  noeud fils_droit ; 
-} ; 
-
+  void *val;
+  noeud fils_gauche;
+  noeud fils_droit;
+};
 
 struct arbre_struct {
-  noeud racine ;
-  void ( * copier ) ( void * val ,
-		      void * * pt )  ; 
-  void ( * detruire ) ( void * * pt ) ; 
-  int ( * comparer ) ( void * val1 ,
-		       void * val2 ) ;
-} ; 
+  noeud racine;
+  void (*copier)(void *val, void **pt);
+  void (*detruire)(void **pt);
+  int (*comparer)(void *val1, void *val2);
+};
 
-
-static noeud noeud_creer ( void * val ,
-			   void ( * copier ) ( void * val ,
-					       void * * pt ) ) {
+/*!
+* \brief Créer un noeud sans enfant et avec pour valeur val.
+* \param val Un pointeur vers la valeur à copier dans le noeud.
+* \param copier Un pointeur vers une fonction pour copier une valeur, un
+* pointeur est indiquer pour la relier
+* \return Un noeud sans enfant et avec pour valeur val.
+*/
+static noeud noeud_creer(void *val, void (*copier)(void *val, void **pt)) {
+  assert(val != NULL);
+  assert(copier != NULL);
+  noeud n = (noeud)malloc(sizeof(struct noeud_struct));
+  copier(val, &(n->val));
+  n->fils_droit = n->fils_gauche = NULL;
 }
-
 
 /* Détruit un noeud et tous les noeuds se trouvant en dessous. */
-static void noeud_detruire_recursivement ( noeud * n_pt ,
-					   void ( * detruire ) ( void * * pt ) ) {
-}
-
+static void noeud_detruire_recursivement(noeud *n_pt,
+                                         void (*detruire)(void **pt)) {}
 
 /* Détruit un noeud sans détruire les noeuds se trouvant en dessous.
- * Il faut donc assurer la correcte structure de l'arbre de recherche après suppression.
+ * Il faut donc assurer la correcte structure de l'arbre de recherche après
+ * suppression.
  */
-static void noeud_detruire_simple ( noeud * const n_pt ,
-				    void ( * detruire ) ( void * * pt ) ) {
-}
+static void noeud_detruire_simple(noeud *const n_pt,
+                                  void (*detruire)(void **pt)) {}
 
+static void noeud_afficher_prefixe(noeud n, FILE *f,
+                                   void (*afficher)(void *val, FILE *f)) {}
 
+static void noeud_afficher_infixe(noeud n, FILE *f,
+                                  void (*afficher)(void *val, FILE *f)) {}
 
-static void noeud_afficher_prefixe ( noeud n ,
-				     FILE * f ,
-				     void ( * afficher ) ( void * val ,
-							   FILE * f ) ) {
-}
+static void noeud_afficher_postfixe(noeud n, FILE *f,
+                                    void (*afficher)(void *val, FILE *f)) {}
 
+int noeud_taille(noeud n) {}
 
-static void noeud_afficher_infixe ( noeud n ,
-				    FILE * f ,
-				    void ( * afficher ) ( void * val ,
-							  FILE * f ) ) {
-}
+arbre arbre_creer(void (*copier)(void *val, void **pt),
+                  void (*detruire)(void **pt),
+                  int (*comparer)(void *val1, void *val2)) {}
 
-
-static void noeud_afficher_postfixe ( noeud n ,
-				      FILE * f ,
-				      void ( * afficher ) ( void * val ,
-							    FILE * f ) ) {
-}
-
-
-int noeud_taille ( noeud n ) {
-}
-
-
-arbre arbre_creer ( void ( * copier ) ( void * val ,
-					void * * pt ) ,
-		    void ( * detruire ) ( void * * pt ) ,
-		    int ( * comparer ) ( void * val1 ,
-					 void * val2 ) ) {
-}
-
-
-void arbre_detruire ( arbre * a ) {
-}
-
+void arbre_detruire(arbre *a) {}
 
 /*
  * Simplifier largement insertion, recherche et suppression
  * (factorisation de code !)
  */
-static noeud * arbre_chercher_position ( arbre a ,
-					 void * val ) {
-}
+static noeud *arbre_chercher_position(arbre a, void *val) {}
 
+void arbre_insertion(arbre a, void *val) {}
 
-void arbre_insertion ( arbre a ,
-		       void * val ) {
-}
+void arbre_afficher_prefixe(arbre a, FILE *f,
+                            void (*afficher)(void *val, FILE *f)) {}
 
+void arbre_afficher_infixe(arbre a, FILE *f,
+                           void (*afficher)(void *val, FILE *f)) {}
 
-void arbre_afficher_prefixe ( arbre a ,
-			      FILE * f ,
-			      void ( * afficher ) ( void * val ,
-						    FILE * f ) ) {
-}
+void arbre_afficher_postfixe(arbre a, FILE *f,
+                             void (*afficher)(void *val, FILE *f)) {}
 
-void arbre_afficher_infixe ( arbre a ,
-			     FILE * f ,
-			     void ( * afficher ) ( void * val ,
-						   FILE * f ) ) {
-}
+bool arbre_est_vide(arbre a) {}
 
-void arbre_afficher_postfixe ( arbre a ,
-			       FILE * f ,
-			       void ( * afficher ) ( void * val ,
-						     FILE * f ) ) {
-}
+int arbre_taille(arbre a) {}
 
+void *arbre_rechercher(arbre a, void *val) {}
 
-bool arbre_est_vide ( arbre a ) {
-}
-
-
-
-int arbre_taille ( arbre a ) {
-}
-
-
-
-void * arbre_rechercher ( arbre a ,
-			  void * val ) {
-}
-
-
-void arbre_supprimer ( arbre a ,
-		       void * val ) {
-}
-
-
+void arbre_supprimer(arbre a, void *val) {}
--- arbres.h
+++ arbres.h
@@ -1,16 +1,17 @@
-# ifndef __ARBRES_H
-# define __ARBRES_H
+#ifndef __ARBRES_H
+#define __ARBRES_H
 
 #include <stdbool.h>
-
+#include <stdio.h>
 
 /*!
  * \file
  * \brief Ce module permet de manipuler des arbre binaire de recherche.
  *
- * La stucture est à deux niveaux: 
- * \li une structure accessible par pointeur/référence de l'extérieur avec les pointeurs sur les bonnes fonctions, et
- * \li une structure complétement cachée qui fait l'arbre binaire. 
+ * La stucture est à deux niveaux:
+ * \li une structure accessible par pointeur/référence de l'extérieur avec les
+ * pointeurs sur les bonnes fonctions, et
+ * \li une structure complétement cachée qui fait l'arbre binaire.
  *
  * Une fonction de comparaison prend deux valeurs en argument et renvoie
  * \li une valeur <= 0 si la première est avant la seconde,
@@ -18,100 +19,89 @@
  * \li sinon la première est après la seconde.
  * Ceci bien sûr au sens de la comparaison concernée.
  *
- * Selon la nature de cette fonction (et des valeurs insérées), les valeurs manipulées peuvent être simple ou des couples (clé,valeur) avec la comparaison sur les clés uniquement. 
+ * Selon la nature de cette fonction (et des valeurs insérées), les valeurs
+ * manipulées peuvent être simple ou des couples (clé,valeur) avec la
+ * comparaison sur les clés uniquement.
  * On réalise alors un tableau associatif.
  *
  * \copyright PASD
  * \version 2016
  */
 
-
 /*! Les structure sont manipulés par pointer/référence.
  */
-typedef struct arbre_struct * arbre;
+typedef struct arbre_struct *arbre;
 
-
-
-/*! 
+/*!
  * Cette fonction retourne un arbre vide.
- * \param copier une fonction pour copier une valeur, un pointeur est indiqué pour la relier
- * \param detruire une fonction pour détruire une valeur, le pointeur est indiqué pour la mettre à jour
+ * \param copier une fonction pour copier une valeur, un pointeur est indiqué
+ * pour la relier
+ * \param detruire une fonction pour détruire une valeur, le pointeur est
+ * indiqué pour la mettre à jour
  * \param comparer une fonction utilisée pour ordonner les élements.
  * \pre ces fonctions doivent être définies
  * \return un arbre vide prêt à recevoir des valeurs.
  */
-arbre arbre_creer ( void ( * copier ) ( void * val ,
-					void * * pt ) ,
-		    void ( * detruire ) ( void * * pt ) ,
-		    int ( * comparer ) ( void * val1 ,
-					 void * val2 ) ) ;
+arbre arbre_creer(void (*copier)(void *val, void **pt),
+                  void (*detruire)(void **pt),
+                  int (*comparer)(void *val1, void *val2));
 
 /*!
  * Cette fonction détruit entièrement un arbre.
  * Le pointeur indiqué est mis à NULL.
  */
-void arbre_detruire ( arbre * a ) ;
-
+void arbre_detruire(arbre *a);
 
 /*!
- * Cette fonction permet de savoir si un arbre est vide (ne contient aucune valeur) ou non.
+ * Cette fonction permet de savoir si un arbre est vide (ne contient aucune
+ * valeur) ou non.
  * \param a arbre à tester
  * \return vrai ssi l'arbre ne contient pas de valeur.
  */
-bool arbre_est_vide ( arbre a ) ;
+bool arbre_est_vide(arbre a);
 
 /*!
- * Cette fonction insère (une copie) d'une valeur dans un arbre binaire de recherche.
+ * Cette fonction insère (une copie) d'une valeur dans un arbre binaire de
+ * recherche.
  * Si val est déjà présent dans l'arbre on ne l'insère pas une deuxième fois.
- * \param 
+ * \param
  * \pre ¿ à compléter ?
  */
-void arbre_insertion ( arbre a ,
-		       void * val ) ;
-
+void arbre_insertion(arbre a, void *val);
 
 /*!
- * Cette fonction affiche les valeurs contenues dans un arbre sur une ligne selon un parcours postfixe.
+ * Cette fonction affiche les valeurs contenues dans un arbre sur une ligne
+ * selon un parcours postfixe.
  * \param f flux où afficher
  * \param afficher fonction pour afficher une valeur.
  */
-void arbre_afficher_postfixe ( arbre a ,
-			       FILE * f ,
-			       void ( * afficher ) ( void * val ,
-						     FILE * f ) 
-			       ) ; 
-
+void arbre_afficher_postfixe(arbre a, FILE *f,
+                             void (*afficher)(void *val, FILE *f));
 
 /*!
- * Cette fonction affiche les valeurs contenues dans un arbre sur une ligne selon un parcours prefixe.
+ * Cette fonction affiche les valeurs contenues dans un arbre sur une ligne
+ * selon un parcours prefixe.
  * \param f flux où afficher
  * \param afficher fonction pour afficher une valeur.
  */
-void arbre_afficher_prefixe ( arbre a ,
-			      FILE * f ,
-			      void ( * afficher ) ( void * val ,
-						    FILE * f ) ) ; 
+void arbre_afficher_prefixe(arbre a, FILE *f,
+                            void (*afficher)(void *val, FILE *f));
 
 /*!
- * Cette fonction affiche les valeurs contenues dans un arbre sur une ligne selon un parcours infixe.
+ * Cette fonction affiche les valeurs contenues dans un arbre sur une ligne
+ * selon un parcours infixe.
  * \param f flux où afficher
  * \param afficher fonction pour afficher une valeur.
  */
-void arbre_afficher_infixe ( arbre a ,
-			     FILE * f ,
-			     void ( * afficher ) ( void * val ,
-						   FILE * f ) ) ; 
-
+void arbre_afficher_infixe(arbre a, FILE *f,
+                           void (*afficher)(void *val, FILE *f));
 
 /*!
  * Cette fonction permet de connaître le nombre de valeurs dans l'arbre.
  * (cela se fait par un calcul récursif.)
  * \return le nombre de noeuds qui compose l'arbre.
  */
-int arbre_taille ( arbre a ) ;
-
-
-
+int arbre_taille(arbre a);
 
 /*
  * Cette fonction permet de suppression une val dans l'arbre.
@@ -119,19 +109,16 @@ int arbre_taille ( arbre a ) ;
  * \param val valeur à supprimer
  */
 
-void arbre_supprimer ( arbre a ,
-		       void * val ) ;
+void arbre_supprimer(arbre a, void *val);
 
 /*!
- * La fonction renvoie le champ val du noeud trouvé ou NULL si aucun noeud ne contient un champ val de même clé.
+ * La fonction renvoie le champ val du noeud trouvé ou NULL si aucun noeud ne
+ * contient un champ val de même clé.
  * Aucune copie n'est faite.
  * \param a arbre où chercher
  * \param val valeur à chercher
  * \return val dans l'arbre, NULL si non présent.
  */
-void * arbre_rechercher ( arbre a ,
-			  void * val ) ;
-
-
+void *arbre_rechercher(arbre a, void *val);
 
 #endif
